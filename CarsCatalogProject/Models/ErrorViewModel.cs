0cb03ce05632e73f77d35f6bc4542694266e77c6
raissa0122 <simeonovaraisa@gmail.com>
2023-03-10 21:19:40 +0200

    Add project files.

---
+++ CarsCatalogProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CarsCatalogProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
